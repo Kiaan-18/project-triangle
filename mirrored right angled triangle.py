759450a6ce06c186fbbb0ecaec657b8d5abf8b77
Kiaan-18 <kapse.kiaan@gmail.com>
2025-05-10 09:47:04 +0200

    Add files via upload

---
+++ mirrored right angled triangle.py
@@ -0,0 +1,15 @@
+import turtle
+screen=turtle.Screen()
+screen.bgcolor("white")
+pen=turtle.Turtle
+pen=turtle.Turtle()
+pen.penup()
+pen.goto(100,0)
+pen.pendown()
+lenght=100
+pen.right(180)
+pen.forward(lenght)
+pen.left(135)
+pen.forward(lenght*(2**0.5))
+pen.left(135)
+pen.forward(lenght)
